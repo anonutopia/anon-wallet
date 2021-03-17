81495187fab3ef8c5c63725fbbeaf0527cec7281
Pragmatic <cryptopragmatic@protonmail.com>
2021-03-17 02:55:24 +0100

    password modal

--- src/wallet.ts
+++ src/wallet.ts
@@ -1,69 +1,77 @@
-import { Signer } from '@waves/signer';
-import { ProviderSeed } from '@waves/provider-seed';
-import { libs } from '@waves/waves-transactions';
 import $ from "jquery";
 
-const seed = "admit drink family great deposit fade exhibit taste piece tomato because fall invest donor opera";
-const signer = new Signer();
-const provider = new ProviderSeed(seed);
-signer.setProvider(provider);
-
-console.log(seed);
-console.log(signer);
-console.log(provider);
-
-console.log(libs.crypto.address(seed))
+var activeScreen = "home";
 
 $("#receive").on( "click", function() {
+    activeScreen = "receive";
     $("#screen-home").fadeOut(function(){
         $("#screen-receive").fadeIn();
     });
 });
 
 $("#backFromReceive").on( "click", function() {
+    activeScreen = "home";
     $("#screen-receive").fadeOut(function(){
         $("#screen-home").fadeIn();
     });
 });
 
 $("#send").on( "click", function() {
+    activeScreen = "send";
     $("#screen-home").fadeOut(function(){
         $("#screen-send").fadeIn();
     });
 });
 
 $("#backFromSend").on( "click", function() {
+    activeScreen = "home";
     $("#screen-send").fadeOut(function(){
         $("#screen-home").fadeIn();
     });
 });
 
 $("#cards").on( "click", function() {
+    activeScreen = "cards";
     $("#screen-home").fadeOut(function(){
         $("#screen-cards").fadeIn();
     });
 });
 
 $("#backFromCards").on( "click", function() {
+    activeScreen = "home";
     $("#screen-cards").fadeOut(function(){
         $("#screen-home").fadeIn();
     });
 });
 
 $("#settings").on( "click", function() {
-    $("#screen-home").fadeOut(function(){
-        $("#screen-settings").fadeIn();
-    });
+    if (activeScreen != "home") {
+        $("#screen-" + activeScreen).fadeOut(function(){
+            $("#screen-settings").fadeIn();
+            activeScreen = "settings";
+        });
+    } else {
+        activeScreen = "settings";
+        $("#screen-home").fadeOut(function(){
+            $("#screen-settings").fadeIn();
+        });
+    }
 });
 
 $("#backFromSettings").on( "click", function() {
+    activeScreen = "home";
     $("#screen-settings").fadeOut(function(){
         $("#screen-home").fadeIn();
     });
 });
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    $("#loading-page").fadeOut(function(){
-        $("#main-page").fadeIn();
+    $("#page-loading").fadeOut(function(){
+        var page = getPage();
+        $("#page-" + page).fadeIn();
     });
 })
+
+function getPage() {
+    return "main";
+}
