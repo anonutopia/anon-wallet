07bc3cf543e76e82d3c89339b648ba2348751c8d
Pragmatic <cryptopragmatic@protonmail.com>
2021-04-02 22:25:00 +0200

    earnings work

--- src/app.ts
+++ src/app.ts
@@ -15,11 +15,16 @@ class Wallet {
     private signer;
     private provider;
     private seedSaved;
+
     balanceWaves:number;
     balanceAhrk:number;
     balanceAeur:number;
     balanceAnote:number;
     balanceAint:number;
+
+    earningsWaves:number;
+    earningsAhrk:number;
+    earningsAeur:number;
 
     constructor() { 
         this.address = Cookies.get("address");
@@ -32,6 +37,10 @@ class Wallet {
         this.balanceAeur = 0;
         this.balanceAnote = 0;
         this.balanceAint = 0;
+
+        this.earningsWaves = 0;
+        this.earningsAhrk = 0;
+        this.earningsAeur = 0;
     }
 
     getPage():string {
@@ -61,6 +70,22 @@ class Wallet {
 
     getEarningsScript() {
         var newScript = document.createElement("script");
+        newScript.onload = function() {
+            console.log($("#earningsWaves").val());
+
+            wallet.earningsWaves = parseInt(String($("#earningsWaves").val()));
+            wallet.earningsAhrk = parseInt(String($("#earningsAhrk").val()));
+            wallet.earningsAeur = parseInt(String($("#earningsAhrk").val()));
+    
+            $("#accumulatedEarningsAint").html(String((wallet.earningsWaves / SATINBTC).toFixed(8)));
+
+            if (t.lang == "en") {
+                $("#accumulatedEarningsMain").html(String((wallet.earningsAeur / 100).toFixed(2)));
+            } else if (t.lang == "hr") {
+                $("#accumulatedEarningsMain").html(String((wallet.earningsAhrk / AHRKDEC).toFixed(6)));
+            }
+        }
+        newScript.async = false;
         newScript.src = earningsScript + "/" + this.getAddress() + "/earnings.js";
         document.body.appendChild(newScript);
     }
@@ -109,7 +134,7 @@ class Wallet {
     //     }
     // }
 
-    async collectInterest(address:string) {
+    async collectEarnings(address:string) {
         try {
             await this.signer.transfer({
                 amount: 950000,
@@ -125,7 +150,7 @@ class Wallet {
                         setTimeout(function(){
                             $("#pMessage10").fadeOut(function() {
                                 $("#pMessage9").fadeIn();
-                                $("#accumulatedInerest").html("0.000000")
+                                $("#accumulatedEarningsMain").html("0.000000")
                             });
                         }, 2000);
                     });
@@ -136,7 +161,7 @@ class Wallet {
                         setTimeout(function(){
                             $("#pMessage12").fadeOut(function() {
                                 $("#pMessage11").fadeIn();
-                                $("#accumulatedEarnings").html("0.0000")
+                                $("#accumulatedEarningsAint").html("0.0000")
                             });
                         }, 2000);
                     });
@@ -200,7 +225,7 @@ class Wallet {
 
     updateAmount() {
         var currency = $("#sendCurrency").val();
-        console.log(currency);
+
         var amount = 0;
         var dp = this.getDecimalPlaces(String(currency));
         var decimalPlaces = 0;
@@ -227,8 +252,7 @@ class Wallet {
         if (balance < 0) {
             balance = 0;
         }
-        console.log(amount);
-        console.log(this.getFee(String(currency)));
+
         $("#amount").val(String(balance.toFixed(decimalPlaces)));
     }
 
@@ -262,8 +286,6 @@ class Wallet {
 
         var fee = this.getFee(String(currency));
         var feeStr = fee / dp;
-
-        console.log(feeStr);
 
         $("#feePrice").html(String(feeStr.toFixed(decimalPlaces)));
     }
@@ -593,7 +615,8 @@ const AHRKADDRESS = "3PPc3AP75DzoL8neS4e53tZ7ybUAVxk2jAb";
 const AINTADDRESS = "3PBmmxKhFcDhb8PrDdCdvw2iGMPnp7VuwPy"
 
 var activeScreen = "home";
-var earningsScript = "https://aint.anonutopia.com";
+// var earningsScript = "https://aint.anonutopia.com";
+var earningsScript = "https://ca0d82c86a33.ngrok.io";
 var t;
 
 const wallet = new Wallet();
@@ -771,7 +794,7 @@ $("#buttonChangePass").on( "click", function() {
 });
 
 $("#buttonCollect").on( "click", function() {
-    wallet.collectInterest(AHRKADDRESS);
+    wallet.collectEarnings(AHRKADDRESS);
 });
 
 $("#sendCurrency").on( "change", function() {
@@ -780,7 +803,7 @@ $("#sendCurrency").on( "change", function() {
 });
 
 $("#buttonCollectEarnings").on( "click", function() {
-    wallet.collectInterest(AINTADDRESS);
+    wallet.collectEarnings(AINTADDRESS);
 });
 
 $("#buttonCopy").on( "click", function() {
