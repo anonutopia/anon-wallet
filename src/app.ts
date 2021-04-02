6aeb3edb6d2772b823748f81a2befdea143dddef
Pragmatic <cryptopragmatic@protonmail.com>
2021-04-02 15:44:53 +0200

    fix loading balances

--- src/app.ts
+++ src/app.ts
@@ -204,6 +204,7 @@ class Wallet {
 
     updateAmount() {
         var currency = $("#sendCurrency").val();
+        console.log(currency);
         var amount = 0;
         var dp = this.getDecimalPlaces(String(currency));
         var decimalPlaces = 0;
@@ -230,6 +231,8 @@ class Wallet {
         if (balance < 0) {
             balance = 0;
         }
+        console.log(amount);
+        console.log(this.getFee(String(currency)));
         $("#amount").val(String(balance.toFixed(decimalPlaces)));
     }
 
@@ -422,7 +425,7 @@ class Wallet {
                     $("#balance").html(String(balance.toFixed(2)));
                 }
             } else if (asset.assetId == AEUR) {
-                wallet.balanceAeur = asset.amount / 100;
+                wallet.balanceAeur = asset.amount;
                 if (t.lang == "en") {
                     var balance = Math.round(wallet.balanceAeur * 100) / 100;
                     $("#balance").html(String(balance.toFixed(2)));
